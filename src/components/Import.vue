150d45a853a190e1c5fa32de5de239ebc31d33dc
KoenPaas <36813373+KoenPaas@users.noreply.github.com>
2020-12-30 10:33:42 +0100

    Prevent default fix

--- src/components/Import.vue
+++ src/components/Import.vue
@@ -14,7 +14,8 @@
                   ref="importInput"
                   @click="next()"
                   id="input-secret"
-                  v-bind:class="{ error: error, success: ready}">
+                  v-bind:class="{ error: error, success: ready }"
+                  class="prevent">
           <input id="save-button" type="button" @click="newAccount()" value="Save" :disabled="!ready" ref="save">
         </div>
       </div>
@@ -92,6 +93,13 @@ export default {
   mounted () {
     // TODO DELTE ME! Only for test purposes
     // this.num = this.account.secret
+    document.getElementsByClassName('prevent').forEach(element => {
+      element.addEventListener('keydown', e => {
+        if (e.which === 38 || e.which === 40) {
+          e.preventDefault()
+        }
+      })
+    })
 
     store.keys.left = {
       string: 'Back',
